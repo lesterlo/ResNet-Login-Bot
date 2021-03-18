f07e98160033bf19bc4f95e79ec41ab16328e01f
leecat2001a <1155077550@link.cuhk.edu.hk>
2021-03-18 18:57:18 +0800

    Perform logout before login, Add Windows bat for Windows user
    Perform logout before login:
    
    User will regain the 12 hours countdown when running the python script.
    
    Add Windows bat for Windows user:
    
    The Windows bat is useful for Windows user in which they can use a task scheduler to schedule the login task so that they have eternally login status.

--- resnet_login.py
+++ resnet_login.py
@@ -1,5 +1,16 @@
 import requests
 
+def run_logout():
+    logout_url = 'https://securelogin.net.cuhk.edu.hk/auth/logout.html'
+
+    try:
+        r = requests.get(logout_url, allow_redirects = False)
+    except Exception:
+        return -1
+
+    return 0
+
+
 def run_login(usr, pwd):
     login_url = 'http://securelogin.net.cuhk.edu.hk/cgi-bin/login'
     input_data = {'user' : usr , 'password' : pwd , 'cmd' : 'authenticate' , 'Login' : 'Log+In'}
@@ -15,6 +26,15 @@ def run_login(usr, pwd):
 
 if __name__ == "__main__":
     from login_config import * #try to import user
+
+    return_status = run_logout()
+    if return_status == 0:
+        print("Logout Success")
+    elif return_status == -1:
+        print("Request has some error")
+    else:
+        print("Problem")
+    
     return_status = run_login(username, password)
     if return_status == 0:
         print("Login Success")
---
+++ windows.bat
@@ -0,0 +1,1 @@
+python ./resnet_login.py
